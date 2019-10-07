7e4c5931a47f262a6273b92d4379a46123956979
Iurii Prytula <iprytula@insert-ad.insert.intranet>
2019-10-07 14:24:18 +0200

    build colors

---
+++ colors/build/precache-manifest.58112db4fcff912f702ad7e18a848bb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384141f4a27177ef02bbee1541a53848",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2f5654f24b3e20c9660",
+    "url": "/static/css/2.292c8731.chunk.css"
+  },
+  {
+    "revision": "1e012b46332edc25ebe1",
+    "url": "/static/css/main.a23ecb50.chunk.css"
+  },
+  {
+    "revision": "e2f5654f24b3e20c9660",
+    "url": "/static/js/2.3d16e40b.chunk.js"
+  },
+  {
+    "revision": "1e012b46332edc25ebe1",
+    "url": "/static/js/main.f2f92c1f.chunk.js"
+  },
+  {
+    "revision": "2c541cf8f95903748fe9",
+    "url": "/static/js/runtime-main.d112a40e.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/static/media/bg.77428cd4.svg"
+  }
+]);
